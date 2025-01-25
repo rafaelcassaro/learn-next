4b42c4cbe086743cc66fcf387b89bc16b13ab8d2
Rafael Cassaro Costa <rafaelcassacosta@hotmail.com>
2025-01-25 20:47:26 -0300

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter , Dosis, Playwrite_VN, Lusitana } from 'next/font/google';
+
+
+export const inter = Inter({ subsets: ['latin'] });
+export const dosis = Dosis({ subsets: ['latin'], weight: ['400' ,'700'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400' ,'700'] });
+export const playwrite_VN = Playwrite_VN();
